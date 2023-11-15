20b5aba85ef95bbe90343438ece13eb43f5fb7b9
salargholami <salargholami021@gmail.com>
2023-11-15 15:33:45 +0330

    CharacterList and Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import { allCharacters } from "../data/data";
+import CharacterDetail from "./components/CharacterDetail";
+import CharacterList from "./components/CharacterList";
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <div className="main">
+        <CharacterList characters={allCharacters} />
+        <CharacterDetail />
+      </div>
+    </div>
+  );
+}
+
+export default App;
