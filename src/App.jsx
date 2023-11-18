ca2200b650fcbd34e9499180956edf6e298e6b1d
salargholami <salargholami021@gmail.com>
2023-11-18 15:52:33 +0330

    Adding loading state in

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,15 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [characters, setCharacters] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const res = await fetch("https://rickandmortyapi.com/api/character");
       const data = await res.json();
       setCharacters(data.results.slice(0, 5));
+      setIsLoading(false);
     }
     fetchData();
   }, []);
@@ -33,7 +36,7 @@ function App() {
         <SearchResult numOfResult={characters.length} />
       </Navbar>
       <Main>
-        <CharacterList characters={characters} />
+        <CharacterList characters={characters} isLoading={isLoading}/>
         <CharacterDetail />
       </Main>
     </div>
