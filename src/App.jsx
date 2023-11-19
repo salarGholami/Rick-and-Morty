cd0fe68c7db3301c50e562b33a59834a89894412
salargholami <salargholami021@gmail.com>
2023-11-19 14:13:46 +0330

    Error handling

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,47 @@ import CharacterDetail from "./components/CharacterDetail";
 import CharacterList from "./components/CharacterList";
 import Navbar, { SearchResult } from "./components/Navbar";
 import { useEffect, useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [characters, setCharacters] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  useEffect(() => {
-    async function fetchData() {
-      setIsLoading(true);
-      const res = await fetch("https://rickandmortyapi.com/api/character");
-      const data = await res.json();
-      setCharacters(data.results.slice(0, 5));
-      setIsLoading(false);
-    }
-    fetchData();
-  }, []);
-
   // useEffect(() => {
-  //   fetch("https://rickandmortyapi.com/api/character")
-  //     .then((res) => res.json())
-  //     .then((data) => setCharacters(data.results.slice(0, 3)));
+  //   async function fetchData() {
+  //     try {
+  //       setIsLoading(true);
+  //       const res = await fetch("https://rickandmortyapi.com/api/character");
+  //      if(!res.ok) throw new Error("something is wrong")
+  //       const data = await res.json();
+  //       setCharacters(data.results.slice(0, 5));
+  //       setIsLoading(false);
+  //     } catch (err) {
+  //       console.log(err.message);
+  //       toast.error(err.message);
+  //     } finally {
+  //       setIsLoading(false);
+  //     }
+  //   }
+  //   fetchData();
   // }, []);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => {
+        if (!res.ok) throw new Error("something is wrong");
+        return res.json();
+      })
+      .then((data) => {
+        setCharacters(data.results.slice(0, 3));
+        // setIsLoading(false);
+      })
+      .catch((err) => {
+        toast.error(err.message);
+      })
+      .finally(() => setIsLoading(false));
+  }, []);
 
   // then catch => async await . ???
   // async function test(){}
@@ -32,11 +52,12 @@ function App() {
 
   return (
     <div className="App">
+      <Toaster />
       <Navbar>
         <SearchResult numOfResult={characters.length} />
       </Navbar>
       <Main>
-        <CharacterList characters={characters} isLoading={isLoading}/>
+        <CharacterList characters={characters} isLoading={isLoading} />
         <CharacterDetail />
       </Main>
     </div>
